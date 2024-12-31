dc6ed8b6abf3baab8d1bb3161c5c7d7e57a50e30
Amanjainji <ajamanjain27@gmail.com>
2024-12-31 14:39:50 +0530

    discord clone initialise

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        lightBlue: "#404EED",
+      },
+      fontFamily: {
+        ggsans: ["gg-sans"],
+        Whitney: ["Whitney"],
+        Ginto: ["Ginto"],
+        ggSans: ["ggSans"],
+      },
+      spacing: {
+        30: "120px",
+      },
+    },
+  },
+  plugins: [],
+};
